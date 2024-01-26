469380bed86deb512d2c5e18b98edf0ca9c389b4
gautambhetanabhotla <143905036+gautambhetanabhotla@users.noreply.github.com>
2024-01-26 22:51:52 +0530

    periodic commit, reanimation still not working

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 var button = document.getElementById('dlmodetoggle');
 var stylesheet = document.getElementById('STYLE');
 
+function reanimate() {
+	if(document.getElementsByTagName('body').style.animation == "transitionin") {
+		document.getElementsByTagName('body').style.animation = "transitionin2";
+	}
+	else {
+		document.getElementsByTagName('body').style.animation = "transitionin";
+	}
+}
+
 function toggleDLmode() {
 	if(stylesheet.getAttribute('href') == 'lightmode.css') {
 		stylesheet.setAttribute('href', 'darkmode.css');
+		reanimate();
 	}
 	else {
 		stylesheet.setAttribute('href', 'lightmode.css');
+		reanimate();
 	}
 }
 

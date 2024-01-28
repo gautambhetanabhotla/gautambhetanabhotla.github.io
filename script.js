2e2a0c57bee79858be2c43fb06477249438fc1b2
gautambhetanabhotla <gautam.bhetanabhotla@students.iiit.ac.in>
2024-01-28 19:33:13 +0530

    animations working, so dumb

--- script.js
+++ script.js
@@ -2,25 +2,12 @@ var button = document.getElementById('dlmodetoggle');
 var stylesheet = document.getElementById('STYLE');
 var BODY2 = document.querySelector('body');
 
-function reanimate() {
-	if(BODY2.style.animationName == 'transitionin') {
-		document.getElementById('scriptchecker').innerHTML = 'script worked';
-		BODY2.style.animationName = 'transitionin2';
-	}
-	else {
-		document.getElementById('scriptchecker').innerHTML = 'script worked';
-		BODY2.style.animationName = 'transitionin';
-	}
-}
-
 function toggleDLmode() {
 	if(stylesheet.getAttribute('href') == 'lightmode.css') {
 		stylesheet.setAttribute('href', 'darkmode.css');
-		reanimate();
 	}
 	else {
 		stylesheet.setAttribute('href', 'lightmode.css');
-		reanimate();
 	}
 }
 

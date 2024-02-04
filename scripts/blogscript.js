ea250ef42bba86597e4340124379939a1834bc1d
gautambhetanabhotla <143905036+gautambhetanabhotla@users.noreply.github.com>
2024-02-05 01:03:35 +0530

    blogs, likes, !comments

--- scripts/blogscript.js
+++ scripts/blogscript.js
@@ -1,15 +1,32 @@
 var buttonBLOG = document.getElementById('dlmodetoggleBLOG');
 var blogstylesheet = document.getElementById('BLOGSTYLE');
+var likebutton = document.getElementById('likebutton');
+var liked = false;
 
-function toggleDLmodeBLOG() {
-	if(blogstylesheet.getAttribute('href') == 'styles/lightmodeblog.css') {
-		blogstylesheet.setAttribute('href', 'styles/darkmodeblog.css');
+function like() {
+	if(!liked) {
+		likebutton.innerHTML = "👍 7";
+		liked = true;
 	}
 	else {
-		blogstylesheet.setAttribute('href', 'styles/lightmodeblog.css');
+		likebutton.innerHTML = "👍 6";
+		liked = false;
+	}
+}
+
+function toggleDLmodeBLOG() {
+	if(blogstylesheet.getAttribute('href') == '/styles/lightmodeblog.css') {
+		blogstylesheet.setAttribute('href', '/styles/darkmodeblog.css');
+	}
+	else {
+		blogstylesheet.setAttribute('href', '/styles/lightmodeblog.css');
 	}
 }
 
 buttonBLOG.onclick = function() {
 	toggleDLmodeBLOG();
+}
+
+likebutton.onclick = function() {
+	like();
 }
